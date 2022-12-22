02d1ed5efc920d1f4c15890bf77b86c0126bfad3
Thai Luong <thailb@protonmail.com>
2022-12-22 13:52:47 +0700

    Feature: Support PostgreSQL.

--- src/test/java/com/thailb/testcontainer/contents/ContentTests.java
+++ src/test/java/com/thailb/testcontainer/contents/ContentTests.java
@@ -1,19 +1,19 @@
 package com.thailb.testcontainer.contents;
 
 import com.thailb.testcontainer.DemoTestApplication;
-import com.thailb.testcontainer.models.entities.CreateThreadDTO;
-import com.thailb.testcontainer.repositories.ThreadRepository;
-import com.thailb.testcontainer.services.ContentService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -27,45 +27,50 @@ import static com.mongodb.assertions.Assertions.assertTrue;
 )
 @Testcontainers
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+@Slf4j
 public class ContentTests {
-
-    private final ThreadRepository threadRepository;
-    private final ContentService systemUnderTest;
-
-    @Autowired
-    public ContentTests(ThreadRepository threadRepository, ContentService contentService) {
-        this.threadRepository = threadRepository;
-        this.systemUnderTest = contentService;
-    }
-
     @Container
     private static final MongoDBContainer mongoDB = new MongoDBContainer(
             DockerImageName.parse("mongo:6-focal"))
             .withExposedPorts(27017)
             .waitingFor(Wait.forHttp("/"));
 
+    @SuppressWarnings("resource")
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:13-alpine")
+                .withExposedPorts(5432);
+
     @DynamicPropertySource
     static void setConnectionProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.data.mongodb.uri", mongoDB::getReplicaSetUrl);
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @BeforeAll
+    public static void setUp() {
+        mongoDB.start();
+        postgreSQLContainer.start();
     }
 
     @AfterAll
     public static void tearDown() {
         mongoDB.stop();
+        postgreSQLContainer.stop();
     }
 
-    @DisplayName("Test Testcontainer instance is up and running.")
+    @DisplayName("Test MongoDB instance is up and running.")
     @Test
-    void startTestContainer_CorrectTestContainerConfigure_Successful() {
+    void startTestContainer_MongoDB_Successful() {
         assertTrue(mongoDB.isRunning());
     }
 
-    @DisplayName("Test insert new threads to blank database.")
+    @DisplayName("Test Postgresql instance is up and running.")
     @Test
-    void createThread_ValidInput_Successful() {
-        CreateThreadDTO threadInput = new CreateThreadDTO("Test Thread");
-        systemUnderTest.createThread(threadInput);
-
-        assertTrue(threadRepository.countAllDocuments() > 0);
+    void startTestContainer_Postgresql_Successful() {
+        assertTrue(postgreSQLContainer.isRunning());
     }
 }
